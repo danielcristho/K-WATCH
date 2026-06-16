cc3734764ec78040a03f642c5b24457b2b47f2c6
danielcristho <pepuhodaniel93.learn@gmail.com>
2026-06-16 22:52:31 +0700

    chore: downgrade instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -289,7 +289,7 @@ resource "aws_instance" "k8s_master" {
 
 resource "aws_instance" "k8s_worker" {
   ami           = "ami-0e2c8caa4b6378d8c" # Ubuntu 22.04 LTS us-east-1
-  instance_type = "t3.2xlarge"            # 8 vCPU, 32GB RAM for workloads + detector
+  instance_type = "t3.large"             # 2 vCPU, 8GB RAM for workloads + detector
   key_name      = var.key_name
   subnet_id     = aws_subnet.k8s_subnet.id
 
@@ -315,7 +315,7 @@ resource "aws_instance" "k8s_worker" {
 # SIEM Instance (Wazuh + ELK)
 resource "aws_instance" "siem" {
   ami           = "ami-0e2c8caa4b6378d8c" # Ubuntu 22.04 LTS us-east-1
-  instance_type = "t3.xlarge"             # 4 vCPU, 16GB RAM for ELK stack
+  instance_type = "t3.large"              # 2 vCPU, 8GB RAM for ELK stack
   key_name      = var.key_name
   subnet_id     = aws_subnet.k8s_subnet.id
 
